8f7d000b8e2049c1565597289fbdcd02234060fc
Mohamed Elsyed <mo.elsyed3@gmail.com>
2023-05-01 20:47:39 +0000

    updated

--- main.py
+++ main.py
@@ -158,4 +158,4 @@ def read_database():
 		'IsCellDefect':isCellDefect})
 
 if __name__ == "__main__":
-    app.run(debug=True , host="0.0.0.0" , port=5000)
+    app.run(debug=True , host="0.0.0.0" ,port=5555)
